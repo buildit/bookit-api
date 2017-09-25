0fe007590a2f2a75580376a0fbccea405fe53351
Andrew Ochsner <andrew.ochsner@wipro.com>
2017-09-25 16:41:24 -0500

    Add a ping controller w/ 1 /v1/ping route plus test
    BKT-26

---
+++ src/main/kotlin/com/buildit/bookit/v1/ping/PingController.kt
@@ -0,0 +1,14 @@
+package com.buildit.bookit.v1.ping
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/ping")
+class PingController {
+    @GetMapping
+    fun ping(): String {
+        return "UP"
+    }
+}
---
+++ src/test/kotlin/com/buildit/bookit/v1/ping/PingControllerTests.kt
@@ -0,0 +1,21 @@
+package com.buildit.bookit.v1.ping
+
+import com.winterbe.expekt.expect
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+
+object PingControllerTests : Spek({
+    describe("/v1/ping")
+    {
+        on("GET")
+        {
+            it("should return UP")
+            {
+                val pingController = PingController()
+                expect(pingController.ping()).to.be.equal("UP")
+            }
+        }
+    }
+})
