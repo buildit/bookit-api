5bba0707a5426ebaf6a9f4f0c4233d2abcc50729
Andrew Tuliszewski <defpearlpilot@yahoo.com>
2017-09-28 14:33:54 -0400

    User different annotation for test wiring
    BKIT-24

--- src/test/kotlin/com/buildit/bookit/v1/ping/PingControllerMockMvcTests.kt
+++ src/test/kotlin/com/buildit/bookit/v1/ping/PingControllerMockMvcTests.kt
@@ -4,8 +4,7 @@ import org.hamcrest.Matchers.equalToIgnoringCase
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
@@ -13,8 +12,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPat
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @ExtendWith(SpringExtension::class)
-@AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest(PingController::class)
 class PingControllerMockMvcTests {
     @Autowired
     private lateinit var mockMvc: MockMvc
