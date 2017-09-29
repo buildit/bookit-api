d8e31caffa611e0b7b40dcb9c48c904c9605a7a3
Andrew Ochsner <andrew.ochsner@wipro.com>
2017-09-29 08:57:37 -0500

    Add swagger
    BKIT-26

--- src/main/kotlin/com/buildit/bookit/BookitApplication.kt
+++ src/main/kotlin/com/buildit/bookit/BookitApplication.kt
@@ -3,10 +3,26 @@ package com.buildit.bookit
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 @SpringBootApplication
+@EnableSwagger2
 class BookitApplication
 
 fun main(args: Array<String>) {
     SpringApplication.run(BookitApplication::class.java, *args)
+}
+
+@Bean
+fun api(): Docket {
+    return Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build()
 }
