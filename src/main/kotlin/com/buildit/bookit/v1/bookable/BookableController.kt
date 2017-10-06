9ffc238c4b06f129041bdccc8a6babfc3a51d99a
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-10-06 16:18:27 -0500

    Fix bad path for /bookable
    AMO BKIT-24

--- src/main/kotlin/com/buildit/bookit/v1/bookable/BookableController.kt
+++ src/main/kotlin/com/buildit/bookit/v1/bookable/BookableController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1/booking")
+@RequestMapping("/v1/bookable")
 class BookableController
 {
     val theBookable = Bookable(1, 1, "The best bookable ever")
