6594b753eccfd2ba3b5a054eb40829748d48341a
Andrew Tuliszewski <defpearlpilot@yahoo.com>
2017-10-10 11:28:34 -0400

    Blanket enable CORS for now
    BKIT-26

--- src/main/kotlin/com/buildit/bookit/v1/booking/BookingController.kt
+++ src/main/kotlin/com/buildit/bookit/v1/booking/BookingController.kt
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.CrossOrigin
 import java.time.ZonedDateTime
 
+@CrossOrigin(origins = arrayOf("*"))
 @RestController
 @RequestMapping("/v1/booking")
 class BookingController
---
+++ src/test/kotlin/com/buildit/bookit/v1/location/LocationControllerMockMvcTests.kt
@@ -0,0 +1,32 @@
+package com.buildit.bookit.v1.location
+
+import com.buildit.bookit.v1.ping.PingController
+import org.hamcrest.Matchers
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@ExtendWith(SpringExtension::class)
+@WebMvcTest(LocationController::class)
+class LocationControllerMockMvcTests
+{
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun location() {
+        // arrange
+        // act
+        val result = mockMvc.perform(MockMvcRequestBuilders.get("/v1/location/1"))
+
+        // assert
+        result.andExpect(MockMvcResultMatchers.status().isOk)
+        result.andExpect(MockMvcResultMatchers.jsonPath<String>("$.locationName",
+                                                                Matchers.equalToIgnoringCase("The best location ever")))
+    }
+}
