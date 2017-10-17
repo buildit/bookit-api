aa6c0ea5571e4336aab69ac21134f716ed836970
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-10-16 21:15:11 -0500

    Add a /ping/error endpoint to test log error alarming
    BKIT-28

--- src/main/kotlin/com/buildit/bookit/v1/ping/PingController.kt
+++ src/main/kotlin/com/buildit/bookit/v1/ping/PingController.kt
@@ -1,5 +1,6 @@
 package com.buildit.bookit.v1.ping
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -10,12 +11,22 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/v1/ping", "/")
 class PingController {
+    private val logger = LoggerFactory.getLogger(PingController::class.java)
     /**
      * Gets ping information
      */
     @GetMapping
     fun ping(): Ping {
         return Ping()
+    }
+
+    /**
+     * Gets ping information
+     */
+    @GetMapping("error")
+    fun error(): Ping {
+        logger.error("PING ERROR")
+        throw RuntimeException("PING EXCEPTION")
     }
 
     /**
