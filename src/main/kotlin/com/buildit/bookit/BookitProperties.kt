0da56f2462c8cf9c41fc47c64219edbf74dc5a6d
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-10-23 23:18:27 -0500

    Don't serialize db password
    BKIT-65

--- src/main/kotlin/com/buildit/bookit/BookitProperties.kt
+++ src/main/kotlin/com/buildit/bookit/BookitProperties.kt
@@ -1,11 +1,12 @@
 package com.buildit.bookit
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 @ConfigurationProperties("bookit")
 data class BookitProperties (
     var databaseUser: String = "",
-    var databasePassword: String = "",
+    @JsonIgnore var databasePassword: String = "",
     var databaseUrl: String = "jdbc:h2:./bookit-db",
     var databaseDriver: String = "org.h2.Driver"
 )
