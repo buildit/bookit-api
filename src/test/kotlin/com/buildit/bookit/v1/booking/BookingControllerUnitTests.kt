497a1d4da3b53afcad53fec4d5c6c9ff476de4e8
Mike Thomas <mathomas@users.noreply.github.com>
2017-10-31 16:46:19 -0600

    Regular JUnit test, all cases converted, plus one.  Probably needs refactoring.

--- src/test/kotlin/com/buildit/bookit/v1/booking/BookingControllerUnitTests.kt
+++ src/test/kotlin/com/buildit/bookit/v1/booking/BookingControllerUnitTests.kt
@@ -1,6 +1,14 @@
 package com.buildit.bookit.v1.booking
 
 import com.buildit.bookit.v1.booking.dto.Booking
+import com.buildit.bookit.v1.booking.dto.BookingRequest
+import com.buildit.bookit.v1.location.LocationRepository
+import com.buildit.bookit.v1.location.bookable.BookableRepository
+import com.buildit.bookit.v1.location.bookable.InvalidBookable
+import com.buildit.bookit.v1.location.bookable.dto.Bookable
+import com.buildit.bookit.v1.location.dto.Location
+import com.natpryce.hamkrest.assertion.assertThat
+import com.natpryce.hamkrest.throws
 import com.nhaarman.mockito_kotlin.doReturn
 import com.nhaarman.mockito_kotlin.mock
 import com.winterbe.expekt.expect
@@ -8,19 +16,15 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
-import org.springframework.test.context.junit.jupiter.SpringExtension
 import java.time.Clock
 import java.time.LocalDateTime
+import java.time.ZoneId
 
-/**
- * Booking controller spring mvc integration tests
- */
-@ExtendWith(SpringExtension::class)
 class BookingControllerUnitTests {
+    private val clock: Clock = Clock.systemUTC()
 
     @BeforeEach
-    fun setupMocks() {
+    fun setup() {
     }
 
     @AfterEach
@@ -28,29 +32,112 @@ class BookingControllerUnitTests {
     }
 
     @Nested
-    inner class GetBookings {
+    inner class Get {
+        lateinit var bookingRepo: BookingRepository
 
         @BeforeEach
-        fun setupMock() {
-        }
-
-        @Test
-        fun `getAllBookings returns all existing bookings`() {
-            val clock = Clock.systemUTC()
-
+        fun setup() {
             val startDateTime = LocalDateTime.parse("2017-09-26T09:00:00")
             val endDateTime = LocalDateTime.parse("2017-09-26T10:00:00")
-
-            val mockRepo = mock<BookingRepository> {
+            bookingRepo = mock {
                 on { getAllBookings() }.doReturn(
                     listOf(
-                        Booking(1, 2, "Booking", startDateTime, endDateTime),
-                        Booking(3, 4, "Another Booking", startDateTime, endDateTime)
+                        Booking(1, 10, "Booking", startDateTime, endDateTime),
+                        Booking(2, 20, "Another Booking", startDateTime, endDateTime)
                     )
                 )
             }
-            val bookings = BookingController(mockRepo, mock {}, mock {}, clock).getAllBookings().body
+        }
+
+        @Test
+        fun `getAllBookings() returns all existing bookings`() {
+            val bookings = BookingController(bookingRepo, mock {}, mock {}, clock).getAllBookings().body
             expect(bookings.size).to.be.equal(2)
+        }
+
+        @Test
+        fun `getBooking() for existing booking returns that booking`() {
+            val booking = BookingController(bookingRepo, mock {}, mock {}, clock).getBooking(1)
+            expect(booking.id).to.be.equal(1)
+        }
+
+        @Test
+        fun `getBooking() for nonexistent booking throws exception`() {
+            fun action() = BookingController(bookingRepo, mock {}, mock {}, clock).getBooking(3)
+            assertThat({ action() }, throws<BookingNotFound>())
+        }
+    }
+
+    @Nested
+    inner class Create {
+        private lateinit var bookingController: BookingController
+        private lateinit var locationRepo: LocationRepository
+
+        private val start = LocalDateTime.now(ZoneId.of("America/New_York")).plusHours(1)
+        private val end = start.plusHours(1)
+        private val createdBooking = Booking(1, 999999, "MyRequest", start, end)
+
+        @BeforeEach
+        fun setup() {
+            locationRepo = mock {
+                on { getLocations() }.doReturn(listOf(
+                    Location(1, "NYC", "America/New_York"),
+                    Location(2, "LON", "Europe/London")
+                ))
+            }
+
+            val bookingRepository = mock<BookingRepository> {
+                on { insertBooking(999999, "MyRequest", start, end) }.doReturn(createdBooking)
+                on { getAllBookings() }.doReturn(listOf(
+                    Booking(1, 999999, "Before", start.minusHours(1), end.minusHours(1)),
+                    Booking(2, 999999, "After", start.plusHours(1), end.plusHours(1))
+                ))
+            }
+            val bookableRepo = mock<BookableRepository> {
+                on { getAllBookables() }.doReturn(listOf(Bookable(999999, 1, "Bookable", true)))
+            }
+
+            bookingController = BookingController(bookingRepository, bookableRepo, locationRepo, clock)
+
+        }
+
+        @Test
+        fun `should create a booking`() {
+            val request = BookingRequest(999999, "MyRequest", start, end)
+
+            val response = bookingController.createBooking(request)
+            val booking = response.body
+
+            expect(booking).to.equal(createdBooking)
+        }
+
+        @Test
+        fun `should validate bookable exists`() {
+            val request = BookingRequest(12345, "MyRequest", start, end)
+            fun action() = bookingController.createBooking(request)
+            assertThat({ action() }, throws<InvalidBookable>())
+        }
+
+        @Test
+        fun `should check that the bookable is available`() {
+            val request = BookingRequest(
+                999999,
+                "MyRequest",
+                start.minusMinutes(30),
+                end.minusMinutes(30))
+            fun action() = bookingController.createBooking(request)
+            assertThat({ action() }, throws<BookableNotAvailable>())
+        }
+
+        @Test
+        fun `should check that the bookable is available - overlap end`() {
+            val request = BookingRequest(
+                999999,
+                "MyRequest",
+                start.plusMinutes(30),
+                end.plusMinutes(30))
+            fun action() = bookingController.createBooking(request)
+            assertThat({ action() }, throws<BookableNotAvailable>())
         }
     }
 }
