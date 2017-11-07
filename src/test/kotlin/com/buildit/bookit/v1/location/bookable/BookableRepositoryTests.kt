a42de651e780ba95a71c83180d5ea1fc225b26c1
Mike Thomas <mathomas@users.noreply.github.com>
2017-11-07 16:15:06 -0700

    Just a harmless change to cause a build.

--- src/test/kotlin/com/buildit/bookit/v1/location/bookable/BookableRepositoryTests.kt
+++ src/test/kotlin/com/buildit/bookit/v1/location/bookable/BookableRepositoryTests.kt
@@ -10,9 +10,6 @@ import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
-/**
- * Booking controller unit tests
- */
 @ExtendWith(SpringExtension::class)
 @JdbcTest
 class BookableRepositoryTests @Autowired constructor(
@@ -20,13 +17,8 @@ class BookableRepositoryTests @Autowired constructor(
 ) {
     @Test
     fun getAllBookables() {
-        // arrange
         val bookableRepository = BookableDatabaseRepository(jdbcTemplate)
-
-        // act
         val bookables = bookableRepository.getAllBookables()
-
-        // assert
         expect(bookables.size).to.be.equal(1)
         expect(bookables).to.contain(Bookable(1, 1, "Red", Disposition()))
     }
