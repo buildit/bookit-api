0b0f4ea23affe04a4c319cfe2ed750930191fb83
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-11-07 23:02:31 -0600

    Cleanup imports
    WIP

--- src/main/kotlin/com/buildit/bookit/v1/location/LocationRepository.kt
+++ src/main/kotlin/com/buildit/bookit/v1/location/LocationRepository.kt
@@ -1,10 +1,11 @@
 package com.buildit.bookit.v1.location
 
 import com.buildit.bookit.v1.location.dto.Location
+import org.springframework.data.repository.CrudRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
 @RepositoryRestResource
-interface LocationRepository : org.springframework.data.repository.CrudRepository<Location, Int> {
+interface LocationRepository : CrudRepository<Location, Int> {
 }
 
 //@Repository
--- src/main/kotlin/com/buildit/bookit/v1/location/bookable/BookableRepository.kt
+++ src/main/kotlin/com/buildit/bookit/v1/location/bookable/BookableRepository.kt
@@ -2,10 +2,11 @@ package com.buildit.bookit.v1.location.bookable
 
 import com.buildit.bookit.v1.location.bookable.dto.Bookable
 import com.buildit.bookit.v1.location.dto.Location
+import org.springframework.data.repository.CrudRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
 @RepositoryRestResource
-interface BookableRepository : org.springframework.data.repository.CrudRepository<Bookable, Int> {
+interface BookableRepository : CrudRepository<Bookable, Int> {
     fun findByLocation(location: Location): List<Bookable>
 }
 
