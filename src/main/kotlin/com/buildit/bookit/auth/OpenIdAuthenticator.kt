ebc1bc60959662be628fb0c88dd54199d3b22e65
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-12-04 17:28:37 -0600

    Verify fake signature from "secret" shared key
    BKIT-120

--- src/main/kotlin/com/buildit/bookit/auth/OpenIdAuthenticator.kt
+++ src/main/kotlin/com/buildit/bookit/auth/OpenIdAuthenticator.kt
@@ -2,11 +2,16 @@ package com.buildit.bookit.auth
 
 import io.jsonwebtoken.JwtException
 import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.SignatureAlgorithm
 import org.slf4j.LoggerFactory
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.GrantedAuthority
 import java.util.ArrayList
+import java.util.Base64
+import javax.crypto.spec.SecretKeySpec
 import javax.servlet.http.HttpServletRequest
+import javax.xml.bind.DatatypeConverter
+
 
 interface JwtAuthenticator {
     fun getAuthentication(jwtToken: String, request: HttpServletRequest): UsernamePasswordAuthenticationToken?
@@ -18,10 +23,6 @@ class OpenIdAuthenticator : JwtAuthenticator {
     @Suppress("ReturnCount")
     override fun getAuthentication(jwtToken: String, request: HttpServletRequest): UsernamePasswordAuthenticationToken? {
         // if (listOf("localhost", "integration").any { request.serverName.startsWith(it) } && jwtToken == "FAKE") {
-        if (listOf("localhost", "integration").any { request.serverName.startsWith(it) }) {
-            log.info("Request token FAKE success.")
-            return UsernamePasswordAuthenticationToken("fakeuser", null, ArrayList<GrantedAuthority>())
-        }
         // parse the token.
         val user: String? = try {
             Jwts.parser()
@@ -29,9 +30,27 @@ class OpenIdAuthenticator : JwtAuthenticator {
                 .parseClaimsJws(jwtToken)
                 .body
                 .subject
-        } catch (e: JwtException) {
-            log.info("Unable to parse token", e)
-            null
+        } catch (e: Exception) {
+            if (listOf("localhost", "integration").any { request.serverName.startsWith(it) }) {
+                log.info("Attempt FAKE token validation.")
+                try {
+
+                    //We will sign our JWT with our ApiKey secret
+                    val apiKeySecretBytes = DatatypeConverter.parseBase64Binary(Base64.getEncoder().encodeToString("secret".toByteArray()))
+                    val signingKey = SecretKeySpec(apiKeySecretBytes, SignatureAlgorithm.HS256.jcaName)
+                    Jwts.parser()
+                        .setSigningKey(signingKey)
+                        .parseClaimsJws(jwtToken)
+                        .body
+                        .subject
+                } catch (ex: JwtException) {
+                    log.info("Unable to parse FAKE token", ex)
+                    null
+                }
+            } else {
+                log.info("Unable to parse token", e)
+                null
+            }
         }
 
         if (user != null) {
