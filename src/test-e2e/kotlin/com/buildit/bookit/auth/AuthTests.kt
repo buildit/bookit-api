82470b2436d623050892f6e6085d6139255e3799
Mike Thomas <mathomas@users.noreply.github.com>
2017-12-05 16:31:43 -0700

    Do a bit of nesting and DRYing.

--- src/test-e2e/kotlin/com/buildit/bookit/auth/AuthTests.kt
+++ src/test-e2e/kotlin/com/buildit/bookit/auth/AuthTests.kt
@@ -6,166 +6,179 @@ import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import org.json.JSONObject
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.boot.web.client.RestTemplateBuilder
-import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.FORBIDDEN
+import org.springframework.http.HttpStatus.OK
+import org.springframework.http.HttpStatus.UNAUTHORIZED
 import org.springframework.http.client.ClientHttpRequestInterceptor
 import java.util.Base64
 import java.util.Date
 import javax.xml.bind.DatatypeConverter
+
+private val s = Base64.getEncoder().encodeToString("secret".toByteArray())
 
 /**
  * Test security
  */
 class `Security E2E Tests` {
     private val testRestTemplate = TestRestTemplate(RestTemplateBuilder().rootUri(Global.URI).build())
+    private val SECRET = DatatypeConverter.parseBase64Binary(Base64.getEncoder().encodeToString("secret".toByteArray()))
 
     @BeforeEach
     fun `clear interceptors`() {
         testRestTemplate.restTemplate.interceptors.clear()
     }
 
-    @Test
-    fun `unparsable token`() {
-        testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
-            request.headers["Authorization"] = "Bearer FooBarBaz"
-            execution.execute(request, body)
-        })
+    @Nested
+    inner class `auth header with` {
+        @Test
+        fun `unparsable token`() {
+            testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
+                request.headers["Authorization"] = "Bearer FooBarBaz"
+                execution.execute(request, body)
+            })
 
-        val response = testRestTemplate.getForEntity("/v1/location", String::class.java)
+            val response = testRestTemplate.getForEntity("/v1/location", String::class.java)
 
-        expect(response.statusCode).to.equal(HttpStatus.UNAUTHORIZED)
+            expect(response.statusCode).to.equal(UNAUTHORIZED)
+        }
+
+        @Test
+        fun `blank token`() {
+            testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
+                request.headers["Authorization"] = "Bearer "
+                execution.execute(request, body)
+            })
+
+            val response = testRestTemplate.getForEntity("/v1/location", String::class.java)
+
+            expect(response.statusCode).to.equal(UNAUTHORIZED)
+        }
     }
 
-    @Test
-    fun `blank token`() {
-        testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
-            request.headers["Authorization"] = "Bearer "
-            execution.execute(request, body)
-        })
+    @Nested
+    inner class `jwt that has` {
+        @Test
+        fun `no signature`() {
+            testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
+                val jwt = Jwts.builder()
+                    .setSubject("fakeuser")
+                    .compact()
+                request.headers["Authorization"] = "Bearer $jwt"
+                execution.execute(request, body)
+            })
 
-        val response = testRestTemplate.getForEntity("/v1/location", String::class.java)
+            val response = testRestTemplate.getForEntity("/v1/location", String::class.java)
 
-        expect(response.statusCode).to.equal(HttpStatus.UNAUTHORIZED)
+            expect(response.statusCode).to.equal(UNAUTHORIZED)
+        }
+
+        @Test
+        fun `invalid signature`() {
+            testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
+                val jwt = Jwts.builder()
+                    .setSubject("fakeuser")
+                    .signWith(SignatureAlgorithm.HS256, SECRET)
+                    .compact()
+                    .dropLast(1)
+
+                request.headers["Authorization"] = "Bearer $jwt"
+                execution.execute(request, body)
+            })
+
+            val response = testRestTemplate.getForEntity("/v1/location", String::class.java)
+
+            expect(response.statusCode).to.equal(UNAUTHORIZED)
+        }
+
+        @Test
+        fun `expired`() {
+            testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
+                val jwt = Jwts.builder()
+                    .setSubject("fakeuser")
+                    .setExpiration(Date(System.currentTimeMillis() - 3600000))
+                    .signWith(SignatureAlgorithm.HS256, SECRET)
+                    .compact()
+
+                request.headers["Authorization"] = "Bearer $jwt"
+                execution.execute(request, body)
+            })
+
+            val response = testRestTemplate.getForEntity("/v1/location", String::class.java)
+
+            expect(response.statusCode).to.equal(UNAUTHORIZED)
+        }
+
+        @Test
+        fun `not-before in future`() {
+            testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
+                val jwt = Jwts.builder()
+                    .setSubject("fakeuser")
+                    .setNotBefore(Date(System.currentTimeMillis() + 3600000))
+                    .signWith(SignatureAlgorithm.HS256, SECRET)
+                    .compact()
+
+                request.headers["Authorization"] = "Bearer $jwt"
+                execution.execute(request, body)
+            })
+
+            val response = testRestTemplate.getForEntity("/v1/location", String::class.java)
+
+            expect(response.statusCode).to.equal(UNAUTHORIZED)
+        }
+
+        @Test
+        fun `bad token behaves as anonymous on selected endpoints`() {
+            testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
+                val jwt = Jwts.builder()
+                    .setSubject("fakeuser")
+                    .signWith(SignatureAlgorithm.HS256, SECRET)
+                    .compact()
+                    .drop(1)
+
+                request.headers["Authorization"] = "Bearer $jwt"
+                execution.execute(request, body)
+            })
+
+            val response = testRestTemplate.getForEntity("/v1/ping", String::class.java)
+
+            expect(response.statusCode).to.equal(OK)
+            expect(JSONObject(response.body).has("user")).to.be.`false`
+        }
     }
 
-    @Test
-    fun `no signature`() {
-        testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
-            val jwt = Jwts.builder()
-                .setSubject("fakeuser")
-                .compact()
-            request.headers["Authorization"] = "Bearer $jwt"
-            execution.execute(request, body)
-        })
 
-        val response = testRestTemplate.getForEntity("/v1/location", String::class.java)
+    @Nested
+    inner class `basic auth` {
+        @Test
+        fun `against API`() {
+            val response = Global.BASIC_AUTH_REST_TEMPLATE.getForEntity("/v1/location", String::class.java)
 
-        expect(response.statusCode).to.equal(HttpStatus.UNAUTHORIZED)
-    }
+            expect(response.statusCode).to.equal(OK)
+        }
 
-    @Test
-    fun `invalid signature`() {
-        testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
-            val apiKeySecretBytes = DatatypeConverter.parseBase64Binary(Base64.getEncoder().encodeToString("secret".toByteArray()))
-            val jwt = Jwts.builder()
-                .setSubject("fakeuser")
-                .signWith(SignatureAlgorithm.HS256, apiKeySecretBytes)
-                .compact().dropLast(1)
+        @Test
+        fun `against Actuator`() {
+            val response = Global.BASIC_AUTH_REST_TEMPLATE.getForEntity("/management/env", String::class.java)
 
-            request.headers["Authorization"] = "Bearer $jwt"
-            execution.execute(request, body)
-        })
-
-        val response = testRestTemplate.getForEntity("/v1/location", String::class.java)
-
-        expect(response.statusCode).to.equal(HttpStatus.UNAUTHORIZED)
-    }
-
-    @Test
-    fun `expired`() {
-        testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
-            val apiKeySecretBytes = DatatypeConverter.parseBase64Binary(Base64.getEncoder().encodeToString("secret".toByteArray()))
-            val jwt = Jwts.builder()
-                .setSubject("fakeuser")
-                .setExpiration(Date(System.currentTimeMillis() - 3600000))
-                .signWith(SignatureAlgorithm.HS256, apiKeySecretBytes)
-                .compact()
-
-            request.headers["Authorization"] = "Bearer $jwt"
-            execution.execute(request, body)
-        })
-
-        val response = testRestTemplate.getForEntity("/v1/location", String::class.java)
-
-        expect(response.statusCode).to.equal(HttpStatus.UNAUTHORIZED)
-    }
-
-    @Test
-    fun `not before now`() {
-        testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
-            val apiKeySecretBytes = DatatypeConverter.parseBase64Binary(Base64.getEncoder().encodeToString("secret".toByteArray()))
-            val jwt = Jwts.builder()
-                .setSubject("fakeuser")
-                .setNotBefore(Date(System.currentTimeMillis() + 3600000))
-                .signWith(SignatureAlgorithm.HS256, apiKeySecretBytes)
-                .compact()
-
-            request.headers["Authorization"] = "Bearer $jwt"
-            execution.execute(request, body)
-        })
-
-        val response = testRestTemplate.getForEntity("/v1/location", String::class.java)
-
-        expect(response.statusCode).to.equal(HttpStatus.UNAUTHORIZED)
-    }
-
-    @Test
-    fun `bad token behaves as anonymous`() {
-        testRestTemplate.restTemplate.interceptors.add(ClientHttpRequestInterceptor { request, body, execution ->
-            val apiKeySecretBytes = DatatypeConverter.parseBase64Binary(Base64.getEncoder().encodeToString("secret".toByteArray()))
-            val jwt = Jwts.builder()
-                .setSubject("fakeuser")
-                .signWith(SignatureAlgorithm.HS256, apiKeySecretBytes)
-                .compact().drop(1)
-
-            request.headers["Authorization"] = "Bearer $jwt"
-            execution.execute(request, body)
-        })
-
-        val response = testRestTemplate.getForEntity("/v1/ping", String::class.java)
-
-        expect(response.statusCode).to.equal(HttpStatus.OK)
-        expect(JSONObject(response.body).has("user")).to.be.`false`
-    }
-
-    @Test
-    fun `basic auth against API`() {
-        val response = Global.BASIC_AUTH_REST_TEMPLATE.getForEntity("/v1/location", String::class.java)
-
-        expect(response.statusCode).to.equal(HttpStatus.OK)
-    }
-
-    @Test
-    fun `basic auth against Actuator`() {
-        val response = Global.BASIC_AUTH_REST_TEMPLATE.getForEntity("/management/env", String::class.java)
-
-        expect(response.statusCode).to.equal(HttpStatus.OK)
+            expect(response.statusCode).to.equal(OK)
+        }
     }
 
     @Test
     fun `bearer auth against Actuator fails`() {
         val response = Global.BEARER_AUTH_REST_TEMPLATE.getForEntity("/management/env", String::class.java)
 
-        expect(response.statusCode).to.equal(HttpStatus.FORBIDDEN)
+        expect(response.statusCode).to.equal(FORBIDDEN)
     }
 
     @Test
     fun `anonymous auth against Actuator fails`() {
         val response = Global.ANONYMOUS_REST_TEMPLATE.getForEntity("/management/env", String::class.java)
 
-        expect(response.statusCode).to.equal(HttpStatus.UNAUTHORIZED)
+        expect(response.statusCode).to.equal(UNAUTHORIZED)
     }
-
 }
