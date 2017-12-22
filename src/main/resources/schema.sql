11ee576c127a5803a9100c0cdc676b3979b7177f
Andrew Ochsner <aochsner@cs.stanford.edu>
2017-12-22 10:52:18 -0600

    Revert "let jpa create the schema"
    This reverts commit a4b0457cbef09b8cb35eeb50d2b93fb18839779b.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,43 @@
+DROP TABLE IF EXISTS booking;
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS bookable;
+DROP TABLE IF EXISTS location;
+
+CREATE TABLE user (
+    id          VARCHAR(36)  NOT NULL,
+    PRIMARY KEY (id),
+    external_id VARCHAR(128) NOT NULL,
+    given_name  VARCHAR(128),
+    family_name VARCHAR(128)
+);
+CREATE INDEX idx_external_id
+    ON user (external_id);
+
+CREATE TABLE bookable (
+    id          VARCHAR(36) NOT NULL,
+    PRIMARY KEY (id),
+    location_id VARCHAR(36) NOT NULL,
+    name        VARCHAR(50) NOT NULL,
+    closed      BOOLEAN     NOT NULL,
+    reason      VARCHAR(50) NOT NULL
+);
+CREATE INDEX IDX_LOCATION_ID
+    ON bookable (location_id);
+
+CREATE TABLE location (
+    id        VARCHAR(36) NOT NULL,
+    PRIMARY KEY (id),
+    name      VARCHAR(50) NOT NULL,
+    time_zone VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE booking (
+    id          VARCHAR(36)  NOT NULL,
+    PRIMARY KEY (id),
+    bookable_id VARCHAR(36)  NOT NULL,
+    subject     VARCHAR(255) NOT NULL,
+    start       DATETIME     NOT NULL,
+    end         DATETIME     NOT NULL,
+    user_id     VARCHAR(36)  NOT NULL
+);
+
